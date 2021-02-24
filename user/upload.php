5f990d499b8440e8f16eb1569ced31c3fb3ffb40
Amira Bouaouina <bouaouinamira@gmail.com>
2021-02-24 21:18:35 +0100

    commit

---
+++ user/upload.php
@@ -0,0 +1,16 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+move_uploaded_file($_FILES["image"]["tmp_name"], "C:\wamp64\www\EventsWebsite-API\uploads/" . $_FILES["image"]["name"]);
+$photo_link = "uploads/".$_FILES["image"]["name"];
+$userId = $_POST['userId'];
+include "../db/db_connect.php";
+
+$req="ALTER TABLE users ADD photo varchar(255)";
+$res=$db->exec($req);
+$sql = "UPDATE users SET photo='$photo_link' where id='$userId'";
+$resu = $db->exec($sql);
+
+echo $res;
+?>
