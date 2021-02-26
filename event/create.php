34490e42bf64885132bd812340d12131d5cdb0bf
Amira Bouaouina <bouaouinamira@gmail.com>
2021-02-26 01:57:00 +0100

    commit

--- event/create.php
+++ event/create.php
@@ -13,23 +13,26 @@ $_POST = json_decode($rest_json, true);
 
    //if( isset($_POST['event_name'])&& isset($_POST['start_date'])&& isset($_POST['end_date'])&&isset($_POST['place'])  &&isset($_POST['description']) &&isset($_POST['creator_id'])){
 
-if( isset($_POST['event_name'])&& isset($_POST['place'])  && isset($_POST['description']) && isset($_POST['creator_id'])){
+if( isset($_POST['event_name']) &&isset($_POST['start_date'])&&isset($_POST['end_date'])&& isset($_POST['place'])  && isset($_POST['description']) && isset($_POST['creator_id'])){
     // Includs database connection
     include "../db/db_connect.php";
 
     // Gets the data from post
     $event_name = $_POST['event_name'];
-    //$start_date = $_POST['start_date'];
-    //$end_date = $_POST['end_date'];
-    $start_date = "datetime('now','localtime')";
-    $end_date = "datetime('now','localtime')";
+    $start = $_POST['start_date'];
+    $end = $_POST['end_date'];
+    $start_date=$start.' 00:00:00';
+    $end_date=$end.' 00:00:00';
+
+    //$start_date = datetime($start,'00:00:00');
+    //$end_date = datetime($end,'00:00:00');
     $place = $_POST['place'];
     $description = $_POST['description'];
     $creator_id = $_POST['creator_id'];
 
     // Makes query with post data
 
-    $query = "INSERT INTO events (event_name, start_date, end_date, place, description, creator_id) VALUES ('$event_name',$start_date,$end_date, '$place', '$description','$creator_id')";
+    $query = "INSERT INTO events (event_name, start_date, end_date, place, description, creator_id) VALUES ('$event_name','$start_date','$end_date', '$place', '$description','$creator_id')";
     // If data inserted then set success message otherwise set error message
     if( $db->exec($query) ){
         http_response_code(200);
--- event/update.php
+++ event/update.php
@@ -8,17 +8,17 @@ header("Content-Type: application/json; charset=UTF-8");
 $rest_json = file_get_contents("php://input"); 
 $_POST = json_decode($rest_json, true);
 
-   if( isset($_POST['id']) &&isset($_POST['event_name'])&& isset($_POST['place'])  &&isset($_POST['description']) ){
+   if( isset($_POST['id'])&&isset($_POST['start_date'])&&isset($_POST['end_date']) &&isset($_POST['event_name'])&& isset($_POST['place'])  &&isset($_POST['description']) ){
     // Includs database connection
     include "../db/db_connect.php";
 
     // Gets the data from post
     $id = $_POST['id'];
     $event_name = $_POST['event_name'];
-    //$start_date = $_POST['start_date'];
-    //$end_date = $_POST['end_date'];
-    $start_date = "datetime('now','localtime')";
-    $end_date = "datetime('now','localtime')";
+    $start_date = $_POST['start_date'];
+    $end_date = $_POST['end_date'];
+    //$start_date = "datetime('now','localtime')";
+    //$end_date = "datetime('now','localtime')";
     $place = $_POST['place'];
     $description = $_POST['description'];
 
